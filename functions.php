f54b06d228fcdcfc3fb25b417271561a4ee73892
dannysummerlinjstartorg <danny.summerlin@jstart.org>
2025-06-12 08:50:29 +0100

    adding more suggested plugins

--- functions.php
+++ functions.php
@@ -18,14 +18,21 @@ add_action( 'wp_enqueue_scripts', 'child_enqueue_resources', 15 );
 require_once get_stylesheet_directory() . '/class-tgm-plugin-activation.php';
 add_action( 'tgmpa_register', function () {
 	$plugins = array(
+		array('name' => 'All in One SEO', 'slug' => 'all-in-one-seo-pack', 'required' => false),
+		array('name' => 'Astra Widgets', 'slug' => 'astra-widgets', 'required' => false),
+		array('name' => 'Azure Groups for WordPress', 'slug' => 'azure-groups-for-wordpress', 'required' => false, 'source' => 'https://github.com/dannysummerlin/azure-groups-for-wordpress/archive/main.zip'),
 		array('name' => 'Content Control', 'slug' => 'content-control', 'required' => false),
+		array('name' => 'Display Posts', 'slug' => 'display-posts-shortcode', 'required' => false),
+		array('name' => 'Duplicate Page', 'slug' => 'duplicate-page', 'required' => false),
 		array('name' => 'Enable Media Replace', 'slug' => 'enable-media-replace', 'required' => false),
 		array('name' => 'Media Library Assistant', 'slug' => 'media-library-assistant', 'required' => false),
+		array('name' => 'MLA Insert Fixit', 'slug' => 'mla-insert-fixit', 'required' => false),
 		array('name' => 'Simple History', 'slug' => 'simple-history', 'required' => false),
 		array('name' => 'Spectra for Astra', 'slug' => 'ultimate-addons-for-gutenberg', 'required' => false),
 		array('name' => 'Wordfence', 'slug' => 'wordfence', 'required' => false),
-		array('name' => 'WP SAML Authentication', 'slug' => 'wp-saml-auth', 'required' => false),
 		array('name' => 'WP Code Lite', 'slug' => 'insert-headers-and-footers', 'required' => false),
+		array('name' => 'WP Rough Notation', 'slug' => 'wp-rough-notation', 'required' => false, 'source' => 'https://github.com/dannysummerlin/wp-rough-notation/archive/main.zip'),
+		array('name' => 'WP SAML Authentication', 'slug' => 'wp-saml-auth', 'required' => false),
 	);
 	$config = array(
 		'id'           => 'jstart-dots',
