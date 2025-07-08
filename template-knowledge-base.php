0e953f5928522e618a6f6cc8717d28a55031c64e
dannysummerlinjstartorg <danny.summerlin@jstart.org>
2025-07-08 11:57:56 +0100

    Update template-knowledge-base.php

--- template-knowledge-base.php
+++ template-knowledge-base.php
@@ -29,7 +29,7 @@ get_header(); ?>
 		<div <?php astra_primary_class(); ?>>
 			<?php
 				if(current_user_can('edit_page'))
-					'<div class="edit_link">'.edit_post_link(__('{Edit}')).'</div>';
+					echo '<div class="edit_link">'.edit_post_link(__('{Edit}')).'</div>';
 				astra_primary_content_top();
 				astra_content_page_loop();
 				astra_primary_content_bottom();
