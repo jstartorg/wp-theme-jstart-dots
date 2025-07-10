c25a14d4c3b72b2025e37cece96e05e4d705526e
dannysummerlinjstartorg <danny.summerlin@jstart.org>
2025-07-10 12:12:04 +0100

    Update template-knowledge-base.php

--- template-knowledge-base.php
+++ template-knowledge-base.php
@@ -8,28 +8,14 @@ get_header(); ?>
         const currentEmail = '<?= wp_get_current_user()->user_email; ?>'
         const currentUsername = '<?= wp_get_current_user()->user_login; ?>'
 </script>
-<style>
-	#kb-content-wrapper {
-		display:flex;
-		column-gap: 1rem;
-		margin: 3vh 7vw;
-		width: 100%;
-		:first-child {
-			flex:1;
-		}
-		:nth-child(2) {
-			flex:3;
-		}
-	}
-</style>
 	<div id="kb-content-wrapper">
 		<div class="ast-left-sidebar">
 			<?php dynamic_sidebar('sidebar_kb'); ?>
 		</div>
 		<div <?php astra_primary_class(); ?>>
 			<?php
-				if(current_user_can('edit_page'))
-					echo '<div class="edit_link">'.edit_post_link(__('{Edit}')).'</div>';
+				if(current_user_can('edit_pages'))
+					echo '<div class="edit_link">'.edit_post_link(__('Edit')).'</div>';
 				astra_primary_content_top();
 				astra_content_page_loop();
 				astra_primary_content_bottom();
