b0796076f079f6831b6d51c5a5c929c08d7f4138
dannysummerlinjstartorg <danny.summerlin@jstart.org>
2025-07-10 12:17:10 +0100

    Update template-smb.php

--- template-smb.php
+++ template-smb.php
@@ -15,7 +15,7 @@ get_header(); ?>
 		<div id="smb-content" <?php astra_primary_class(); ?>>
 			<?php
 				if(current_user_can('edit_pages'))
-					echo '<div class="edit_link">'.edit_post_link(__('Edit')).'</div>';
+					edit_post_link(__('Edit'));
 				astra_primary_content_top();
 				astra_content_page_loop();
 				astra_primary_content_bottom();
