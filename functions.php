5d6361d308410896d7f2a5b23ccc0a9dd7f983e9
dannysummerlinjstartorg <danny.summerlin@jstart.org>
2025-07-24 13:48:29 -0700

    Update functions.php
    adding theme js

--- functions.php
+++ functions.php
@@ -6,11 +6,11 @@
  * @since 1.4.0
  */
 /** Define Constants */
-define( 'CHILD_THEME_JSTART_DOTS_VERSION', '1.0.0' );
+define( 'CHILD_THEME_JSTART_DOTS_VERSION', '1.6.0' );
 /** Enqueue styles */
 function child_enqueue_resources() {
 	wp_enqueue_style( 'jstart-dots-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_JSTART_DOTS_VERSION, 'all' );
-	// wp_enqueue_script( 'jstart-dots-theme-js', get_stylesheet_directory_uri() . '/lib.js', array(), CHILD_THEME_JSTART_DOTS_VERSION, 'true');
+	wp_enqueue_script( 'jstart-dots-theme-js', get_stylesheet_directory_uri() . '/theme.js', array(), CHILD_THEME_JSTART_DOTS_VERSION);
 }
 add_action( 'wp_enqueue_scripts', 'child_enqueue_resources', 15 );
 // Adding sidebars for page templates
